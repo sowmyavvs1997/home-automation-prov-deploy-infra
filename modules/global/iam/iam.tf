7594e97c10fd95e06244d803b6e5f2a22e5902b7
sowmyavvs1997 <sowmya.vvs1997@gmail.com>
2026-01-31 22:42:56 +0530

    IAM terraform code updated

--- modules/global/iam/iam.tf
+++ modules/global/iam/iam.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+}
 data "aws_cloudformation_export" "ecs-role-arn" {
   name = "ECSRoleArn"
 }
@@ -11,50 +19,111 @@ data "aws_cloudformation_export" "ec2-instance-profile" {
 }
 
 #############################################################################
-# ECS EC2 Instance Role
-#############################################################################
-
-resource "aws_iam_role" "ecs_instance" {
-  name = "${var.project_name}-${var.environment}-ecs-instance"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Action    = "sts:AssumeRole"
-      Principal = { Service = "ec2.amazonaws.com" }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_instance" {
-  role       = aws_iam_role.ecs_instance.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
-}
-
-resource "aws_iam_instance_profile" "ecs" {
-  name = "${var.project_name}-${var.environment}-ecs"
-  role        = aws_iam_role.ecs_instance.name
-}
-
-#############################################################################
 # ECS Task Execution Role
 #############################################################################
 
-resource "aws_iam_role" "ecs_task_execution" {
-  name = "${var.project_name}-${var.environment}-ecs-exec-"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Action    = "sts:AssumeRole"
-      Principal = { Service = "ecs-tasks.amazonaws.com" }
-    }]
-  })
+## Create a role which allows ECS containers to perform actions such as write logs, call KMS
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      identifiers = ["ecs-tasks.amazon.com", "elasticloadbalancing.amazon.com", "lambda.amazon.com"]
+      type = "Service"
+     }
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
-  role       = aws_iam_role.ecs_task_execution.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name = var.ecs_exec_iam_role
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  permissions_boundary = "arn:aws:iam::${var.aws_account_id}:policy/amp-permissions-boundary"
+}
+
+#############################################################################
+# ECS Task Execution Role Policy
+#############################################################################
+
+resource "aws_iam_policy" "ecs_execution_policy" {
+  name = "var.ecs_exec_iam_policy"
+  path = "/"
+  description = "Allows ECS containers to execute commands on our behalf"
+
+  policy = <<EOF
+  {
+    "version" : "2012-10-17",
+    "statement" : [{
+        "Effect" : "Allow",
+        "Action" : [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLaterAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "iam:*",
+          "logs:CreateRole",
+          "logs:CreateLogStream",
+          "logs:PutEventLogs",
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "s3:*",
+          "ecs:*"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  }
+  EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  policy_arn = aws_iam_policy.ecs_execution_policy.arn
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+}
+
+#############################################################################
+# Master Role Policy Doc
+#############################################################################
+
+data "aws_iam_policy_document" "auto_tag_master_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      identifiers = ["ecs-tasks.amazonaws.com"]
+      type = "Service"
+    }
+    principals {
+      identifiers = [data.aws_cloudformation_export.ecs-role-arn.value]
+      type = "AWS"
+    }
+  }
+}
+
+#ecs-tasks-role master role
+resource "aws_iam_role" "ecs-tasks-role" {
+  name = var.ecs_tasks_iam_role
+  path = "/"
+  assume_role_policy = data.aws_iam_policy_document.auto_tag_master_role_policy.json
+  permissions_boundary = "arn:aws:iam::${var.aws_account_id}:policy/amp-permissions-boundary"
+}
+
+#Policy 2 - task role
+resource "aws_iam_policy" "s3_service_policy" {
+  name        = var.s3_service_policy
+  path        = "/"
+  description = "Allows ECS Containers to execute s3 api calls on its behalf"
+  policy      = <<EOF
+  {
+     "version": "2012-10-17",
+     "Statement" : [{
+     "Effect": "Allow",
+     "Action" : [
+        "s3:*",
+        "iam:UpdateAssumeRolePolicy",
+        "dynamodb:*",
+        "kms:*"
+    ],
+    "Resource": "*"
+   }]
+}
+EOF
 }
--- modules/global/iam/vars.tf
+++ modules/global/iam/vars.tf
@@ -5,3 +5,8 @@ variable "project_name" {
 variable "environment" {
   type = string
 }
+
+variable "ecs_exec_iam_role" {}
+variable "aws_account_id" {}
+variable "ecs_tasks_iam_role" {}
+variable "s3_service_policy" {}
